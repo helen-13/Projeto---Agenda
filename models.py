8c0027ed09c80a957c3446a607134151e4d2f79e
Helen <71827720+helen-13@users.noreply.github.com>
2021-12-29 23:00:32 -0300

    Adição de alteração e exclusão
    Inserção do HTTP 404 e alteração do Settings em relação ao fuso horário e idioma.

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from datetime import datetime
 
 # Create your models here.
 class Evento(models.Model):
@@ -8,6 +8,7 @@ class Evento(models.Model):
     descricao = models.TextField(blank=True, null=True)
     data_evento = models.DateTimeField(verbose_name='Data do Evento')
     data_criacao = models.DateTimeField(auto_now=True)
+    local_evento = models.TextField(blank=True, null=True)
     usuario = models.ForeignKey(User, on_delete= models.CASCADE)
 
 class Meta:
@@ -17,3 +18,12 @@ def _str_(self):
     return self.titulo
 def get_data_evento(self):
     return self.data_evento.strtime('%d/%m/%y%H Hrs:%M min')
+
+def get_data_input_evento(self):
+    return self.data_evento.strtime('%Y-%m-%dT-%H:%M')
+
+def get_evento_atrasado(self):
+    if self.data_evento < datetime.now():
+        return True
+    else:
+        return False
--- views.py
+++ views.py
@@ -3,6 +3,8 @@ from core.models import Evento
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from datetime import datetime, timedelta
+from django.http.response import Http404
 
 
 # Create your views here.
@@ -28,23 +30,49 @@ def submit_login(request):
 @login_required(login_url='/login/')
 def lista_eventos(request):
     usuario = request.user
-    evento = Evento.objects.filter(usuario=usuario)
+    data_atual = datetime.now() - timedelta(hours=1)
+    evento = Evento.objects.filter(usuario=usuario,
+                                   data_evento__gt=data_atual)
     dados = {'eventos': evento}
     return render(request, 'agenda.html', dados)
 
 @login_required(login_url='/login/')
 def evento(request):
-    return render(request, 'evento.html')
+    id_evento = request.GET.get('id')
+    dados = {}
+    if id_evento:
+        dados['envento']= Evento.objects.get(id=id_evento)
+    return render(request, 'evento.html', dados)
 
 @login_required(login_url='/login/')
 def submit_evento(request):
     if request.POST:
         titulo = request.POST.get('titulo')
         data_evento = request.POST.get('data_evento')
+        local = request.POST.get('local_evento')
         descricao = request.POST.get('descricao')
         usuario = request.user
-        Evento.objects.create(titulo=titulo,
+        id_evento = request.POST.get('id_evento')
+        if id_evento:
+            Evento.objects.filter(id=id_evento).update(titulo=titulo,
+                                                       data_evento=data_evento,
+                                                       descricao=descricao, local=local)
+        else:
+            Evento.objects.create(titulo=titulo,
                           data_evento=data_evento,
                           descricao=descricao,
-                          usuario=usuario)
+                          usuario=usuario, local=local)
     return redirect('/')
+
+@login_required(login_url='/login/')
+def delete_evento(request, id_evento):
+    usuario= request.user
+    try:
+        evento= Evento.objects.get(id=id_evento)
+    except Exception:
+        raise Http404()
+    if usuario == evento.usuario:
+        evento.delete()
+    else:
+        raise Http404()
+    return redirect ('/')
